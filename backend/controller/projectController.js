03ea1199eb4e3ad8deeb361d7fbbb3f155c8b455
Priyanshu Verma <gh0stm0nkey@proton.me>
2023-09-26 11:12:09 +0530

    list all file functionality

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -1,10 +1,8 @@
 const asyncHandler = require('express-async-handler')
 const Project = require('../model/projectModel')
 const getProjects = asyncHandler(async(req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = 10;
-    const projects = await Project.find().skip((page - 1) * pageSize).limit(pageSize);
-    res.status(200).json(projects);
+    const projects = await Project.find()
+    res.status(200).json(projects)
 })
 module.exports = {
     getProjects,
