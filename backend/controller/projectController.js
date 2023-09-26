add5d79d77ad657f9db8b0efe94c6966dbb5a4cc
Priyanshu Verma <gh0stm0nkey@proton.me>
2023-09-26 10:26:06 +0530

    added lean method

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const Project = require('../model/projectModel')
 const getProjects = asyncHandler(async(req, res) => {
-    const projects = await Project.find()
+    const projects = await Project.find().lean()
     res.status(200).json(projects)
 })
 module.exports = {
