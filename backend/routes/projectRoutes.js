e59b64641b7eea0bd4e5487fb347c9851cb21bcf
Priyanshu Verma <gh0stm0nkey@proton.me>
2023-09-26 11:03:08 +0530

    added routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const router = express.Router()
 const { getProjects } = require('../controller/projectController')
-router.route('/').get(getProjects)
+router.route('/list').get(getProjects)
 module.exports = router
